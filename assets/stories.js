ad6798c3c3d042f5063fcda5340d0f474a3980a3
_sukalaper <65320033+sukalaper@users.noreply.github.com>
2021-02-18 12:06:22 +0700

    Update stories.js

--- assets/stories.js
+++ assets/stories.js
@@ -1,7 +1,10 @@
 export default [
-  "Hai. ",
-  "Mata-mata apa yang kaga jelas?",
-  "Jawabannya adalah : KUPING!!!!!!!",
-  "HAHAHAHAHAHAH.",
-  "Kita hanya hampir menjadi, namun tak akan pernah menjadi apapun.",
+  "Hai, Asih. ",
+  "Tolong bertahan demi hal-hal kecil.",
+  "Demi bertemu Musim Hujan di Tahun depan.",
+  "Demi kebangun tengah Malem buat masak Indomie di jam 2 Pagi.",
+  "Demi dengerin lagu Sleeping With Sirens favorit lo.",
+  "Atau, demi liat Ibu sama Bapak senyum.",
+  "Sekali lagi,",
+  "Tolong bertahan sekecil apapun Alesannya.",
 ];
