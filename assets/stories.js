db09ccbb9fb1633a908bc7810659fc349403f863
_sukalaper <65320033+sukalaper@users.noreply.github.com>
2021-03-05 15:50:46 +0700

    Update stories.js

--- assets/stories.js
+++ assets/stories.js
@@ -1,10 +1,10 @@
 export default [
-  "Hai, Asih. ",
-  "Tolong bertahan demi hal-hal kecil.",
-  "Demi bertemu Musim Hujan di Tahun depan.",
-  "Demi kebangun tengah Malem buat masak Indomie di jam 2 Pagi.",
-  "Demi dengerin lagu Sleeping With Sirens favorit lo.",
-  "Atau, demi liat Ibu sama Bapak senyum.",
-  "Sekali lagi,",
-  "Tolong bertahan sekecil apapun Alesannya.",
+  "hai, asih. ",
+  "tolong bertahan demi hal-hal kecil.",
+  "demi bertemu musim hujan di tahun depan.",
+  "demi kebangun tengah malem buat matiin videocall kita yang berjam-jam.",
+  "demi dengerin lagu sleeping with sirens favorit lo.",
+  "atau, demi liat ibu sama bapak senyum.",
+  "sekali lagi,",
+  "tolong bertahan sekecil apapun alesannya.",
 ];
